5ae9618ffeca78bf6170aac45aade71ea59eea15
Chau Hoang Hieu Lam <lamchauhoanghieu@gmail.com>
2024-07-13 19:12:00 +1000

    fix: Debug

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,11 +1,12 @@
 // backend/src/middleware/authorize.js
+const jwt = require('jsonwebtoken');
 function authorize(roles = []) {
     if (typeof roles === 'string') {
       roles = [roles];
     }
   
     return (req, res, next) => {
-      console.log(req.user);
+      console.log(req.headers);
       if (!roles.includes(req.user.role)) {
         return res.status(401).json({ message: 'Unauthorized' });
       }
